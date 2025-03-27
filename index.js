0530c3efca358a9aa688745de375738906da80b6
Adrian <adrian.amoke@gmail.com>
2025-03-28 01:25:54 +0300

    edited index.js

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const songsData = 'https://my-app-backend-in2a.onrender.com/api/songs'
-// Selects elements in the html
 const songList = document.getElementById('songList');
 const songDetails = document.getElementById('songDetails');
 const addSongForm = document.getElementById('addSongForm');
 
-//initializes an empty array to store the song data
 let songs = [];
 
 //Retrieves song data from the server
 function fetchSongs() {
-    fetch(songsData)// makes a get request to the url
-        .then(response => response.json()) // processes the response and and converts it from json to js
+    fetch(songsData)
+        .then(response => response.json())
         .then(data => {
-            songs = data; //assigns the fethced data to the songs array
-            displaySongs();// calls the render function to update the song list displayed on the page
+            songs = data; 
+            displaySongs();
         })
-        .catch(error => console.error('Error fetching songs:', error));//handles errors that occur during fetching
+        .catch(error => console.error('Error fetching songs:', error));
 }
 
 //function to render songs
@@ -29,22 +27,21 @@ function displaySongs() {
         <img src="${song.coverUrl}" alt="${song.title} Cover">
         <span>${song.title} by ${song.artist}</span>
         `;
-        //This adds a custom data attribute to the div to store the index of the song
+        
         songItem.dataset.index = index;
 
-        //event that changes color when song item is hovered over
         songItem.addEventListener('mouseover', () => {
             songItem.style.backgroundColor = '#0B0C10';
         });
-        //resets the background color of the song item 
+        
         songItem.addEventListener('mouseout', () => {
             songItem.style.backgroundColor = '';
         });
-        //click event that calls the showSongDetails function
+        
         songItem.addEventListener('click', () => {
             showSongDetails(song);
         });
-        //This appends the newly created div to the songList element, making it visible on the page
+        
         songList.appendChild(songItem);
     });
 }
@@ -61,20 +58,19 @@ function showSongDetails(song) {
 }
 //Event listener for form submission
 addSongForm.addEventListener('submit', (evt) => {
-    evt.preventDefault(); //prevents the page from refreshing after submission
+    evt.preventDefault(); 
     const title = document.getElementById('songTitle').value;
     const artist = document.getElementById('songArtist').value;
     const album = document.getElementById('songAlbum').value;
     const coverUrl = document.getElementById('coverUrl').value;
-    //Create a new song object using the enteres values
+    
     const newSong = { title, artist, album, coverUrl };
-    //add new song to the songs array
+    
     songs.push(newSong);
-    //updates the displayed song list with the newly added song
+    
     displaySongs();
-    //resets the form fields to their initial empty state after submission
+    
     addSongForm.reset();
 });
 
-//initial fetch of songs
 fetchSongs();
